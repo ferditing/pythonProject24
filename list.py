9fbffb83d4e96a1249717ca8da6874cfc2454155
ferditing <151714811+ferditing@users.noreply.github.com>
2024-03-02 15:31:04 +0300

    show various methods that could be applied to listsliied to

---
+++ list.py
@@ -0,0 +1,13 @@
+my_list = []
+my_list.append(10)
+my_list.append(20)
+my_list.append(30)
+my_list.append(40)
+my_list.insert(1, 15)
+my_list2 = [50, 60, 70]
+my_list.extend(my_list2)
+my_list.pop()
+my_list.sort()
+index_30 = my_list.index(30)
+print("Index of value 30 in my_list:", index_30)
+print("Final list:", my_list)
